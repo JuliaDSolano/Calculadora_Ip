91045e7d279ff1924ee728aaa1655cab5ce3e58e
JuliaDSolano <juliaduartesolano@gmail.com>
2018-07-13 09:26:16 -0300

    Add files via upload

--- Calculadora Ip/classes/class_ipv4.php
+++ Calculadora Ip/classes/class_ipv4.php
@@ -7,156 +7,245 @@ class calc_ipv4
     // O endereço IP
     public $endereco;
 
-    // O cidr
+    // O cidr /...
     public $cidr;
 
     // O endereço IP
     public $endereco_completo;
 
-    /**
-     * O construtor apenas configura as propriedades da classe
-     */
-    public function __construct( $endereco_completo ) {
+    //O status da rede
+
+    public $status;
+
+    //classe do ip
+    public $classe;
+
+    /** construtor apenas configura as propriedades da classe*/
+    public function __construct($endereco_completo)
+    {
         $this->endereco_completo = $endereco_completo;
         $this->valida_endereco();
     }
 
-    /**
-     * Valida o endereço IPv4
-     */
-    public function valida_endereco() {
+    /** Valida o endereço IPv4 */
+
+    public function valida_endereco()
+    {
         // Expressão regular
         $regexp = '/^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\/[0-9]{1,2}$/';
 
         // Verifica o IP/CIDR
-        if ( ! preg_match( $regexp, $this->endereco_completo ) ) {
+        if (!preg_match($regexp, $this->endereco_completo)) {
             return false;
         }
 
         // Separa o IP do prefixo CIDR
-        $endereco = explode( '/', $this->endereco_completo );
+        $endereco = explode('/', $this->endereco_completo);
 
         // CIDR
-        $this->cidr = (int) $endereco[1];
+        $this->cidr = (int)$endereco[1];
 
         // Endereço IPv4
         $this->endereco = $endereco[0];
 
         // Verifica o prefixo
-        if ( $this->cidr > 32 ) {
+        if ($this->cidr > 32) {
             return false;
         }
 
-        // Faz um loop e verifica cada número do IP
-        foreach( explode( '.', $this->endereco ) as $numero ) {
+        // Faz loop e verifica cada número do IP
+        foreach (explode('.', $this->endereco) as $numero) {
 
-            // Garante que é um número
-            $numero = (int) $numero;
+            // verifica que é um número
+            $numero = (int)$numero;
 
             // Não pode ser maior que 255 nem menor que 0
-            if ( $numero > 255 || $numero < 0 ) {
+            if ($numero > 255 || $numero < 0) {
                 return false;
             }
         }
 
-        // IP "válido" (correto)
+        // ip válido (correto)
         return true;
     }
 
-    /* Retorna o endereço IPv4/CIDR */
-    public function endereco_completo() {
-        return ( $this->endereco_completo );
+    /* retorna o endereço IPv4/CIDR */
+    public function endereco_completo()
+    {
+        return ($this->endereco_completo);
     }
 
-    /* Retorna o endereço IPv4 */
-    public function endereco() {
-        return ( $this->endereco );
+    /* retorna o endereço IPv4 */
+    public function endereco()
+    {
+        return ($this->endereco);
     }
 
     /* Retorna o prefixo CIDR */
-    public function cidr() {
-        return ( $this->cidr );
+    public function cidr()
+    {
+        return ($this->cidr);
     }
 
     /* Retorna a máscara de sub-rede */
-    public function mascara() {
-        if ( $this->cidr() == 0 ) {
+    public function mascara()
+    {
+
+        if ($this->cidr() == 0) {
             return '0.0.0.0';
         }
 
         return (
         long2ip(
-            ip2long("255.255.255.255") << ( 32 - $this->cidr )
+            ip2long("255.255.255.255") << (32 - $this->cidr)
         )
         );
     }
 
     /* Retorna a rede na qual o IP está */
-    public function rede() {
-        if ( $this->cidr() == 0 ) {
+    public function rede()
+    {
+        if ($this->cidr() == 0) {
             return '0.0.0.0';
         }
 
         return (
         long2ip(
-            ( ip2long( $this->endereco ) ) & ( ip2long( $this->mascara() ) )
+            (ip2long($this->endereco)) & (ip2long($this->mascara()))
         )
         );
     }
 
     /* Retorna o IP de broadcast da rede */
-    public function broadcast() {
-        if ( $this->cidr() == 0 ) {
+    public function broadcast()
+    {
+        if ($this->cidr() == 0) {
             return '255.255.255.255';
         }
 
         return (
-        long2ip( ip2long($this->rede() ) | ( ~ ( ip2long( $this->mascara() ) ) ) )
+        long2ip(ip2long($this->rede()) | (~(ip2long($this->mascara()))))
         );
     }
 
     /* Retorna o número total de IPs (com a rede e o broadcast) */
-    public function total_ips() {
-        return( pow(2, ( 32 - $this->cidr() ) ) );
+    public function total_ips()
+    {
+        return (pow(2, (32 - $this->cidr())));
     }
 
     /* Retorna os número de IPs que podem ser utilizados na rede */
-    public function ips_rede() {
-        if ( $this->cidr() == 32 ) {
+    public function ips_rede(){
+
+        if ($this->cidr() == 32) {
             return 0;
-        } elseif ( $this->cidr() == 31 ) {
+
+        } elseif ($this->cidr() == 31) {
             return 0;
         }
-
-        return( abs( $this->total_ips() - 2 ) );
+        return (abs($this->total_ips() - 2));
     }
 
     /* Retorna os número de IPs que podem ser utilizados na rede */
-    public function primeiro_ip() {
-        if ( $this->cidr() == 32 ) {
+    public function primeiro_ip()
+    {
+        if ($this->cidr() == 32) {
             return null;
-        } elseif ( $this->cidr() == 31 ) {
+
+        } elseif ($this->cidr() == 31) {
             return null;
-        } elseif ( $this->cidr() == 0 ) {
+
+        } elseif ($this->cidr() == 0) {
             return '0.0.0.1';
         }
 
         return (
-        long2ip( ip2long( $this->rede() ) | 1 )
+        long2ip(ip2long($this->rede()) | 1)
         );
     }
 
     /* Retorna os número de IPs que podem ser utilizados na rede */
-    public function ultimo_ip() {
-        if ( $this->cidr() == 32 ) {
+    public function ultimo_ip()
+    {
+
+        if ($this->cidr() == 32) {
             return null;
-        } elseif ( $this->cidr() == 31 ) {
+
+        } elseif ($this->cidr() == 31) {
             return null;
         }
 
         return (
-        long2ip( ip2long( $this->rede() ) | ( ( ~ ( ip2long( $this->mascara() ) ) ) - 1 ) )
+        long2ip(ip2long($this->rede()) | ((~(ip2long($this->mascara()))) - 1))
         );
     }
-}
 
+    public function statusRede(){
+
+
+//        $octetos = explode('.', $this->endereco);
+
+        if ($this->endereco() == 10) {
+
+            $result = "Privado";
+            return $result;
+
+        } elseif ($this->endereco() == 127) {
+
+            $result = "Reservao/Privado";
+            return $result;
+
+        }elseif($this->endereco() >= 172.16 AND $this->endereco() <= 172.31 ){
+
+            $result = "Privado";
+            return $result;
+
+        }elseif($this->endereco() == 192.168){
+
+            $result = "Privado";
+            return $result;
+
+        }elseif($this->endereco() == 169.254){
+
+            $result = "ZeroConf/Privado";
+            return $result;
+
+        }else{
+
+            $result = "Público";
+            return $result;
+        }
+  }
+
+  public function classe(){
+
+        if($this->endereco() <128){
+
+            $classe = "Classe A";
+            return $classe;
+
+        }elseif($this->endereco() >= 128 AND $this->endereco() <192){
+
+            $classe = "Classe B";
+            return $classe;
+
+        }elseif($this->endereco() >= 192 AND $this->endereco() <224){
+
+            $classe = "Classe C";
+            return $classe;
+
+        }elseif($this->endereco() >= 224 AND $this->endereco() <240){
+
+            $classe = "Classe D";
+            return $classe;
+
+        }else{
+
+            $classe = "Classe E";
+            return $classe;
+
+        }
+  }
+
+
+}
--- Calculadora Ip/index.php
+++ Calculadora Ip/index.php
@@ -28,28 +28,35 @@
 </head>
 <body>
 <header>
+
     <h1>Cálculo de máscara de sub-rede IPv4</h1>
     <p>Esta página tem como intuito mostrar como funciona a classe PHP para Cálculo de máscara de sub-rede IPv4.</p>
     <p>Apenas digitando o IP CIDR</a> (ex. 192.168.0.1/24), a classe fará os cálculos necessários para retornar o endereço IPv4, CIDR,
         Máscara de sub-rede, IP da rede, IP de broadcast da rede, primeiro IP da rede, último IP da rede, número total de IPs da rede,
         e o número de IPs disponíveis para uso.</p>
     <p>Faça um teste:</p>
+
 </header>
 
 <section>
     <h1>Calcular máscara de sub-rede IP</h1>
 
     <form method="POST">
+
         <b style="color: #77bfb7">IP/CIDR</b> <small>(Ex.: 192.168.0.1/24)</small> <br>
         <input style="border:1px solid #77bfb7; line-height: 2; padding: 0 5px; width: 200px;" type="text" name="ip" value="<?php echo @$_POST['ip'];?>">
         <input style="border:1px solid #77bfb7; background: #77bfb7; color: #fff; font-weight: 700; cursor: pointer; line-height: 2; padding: 0 5px;" type="submit" value="Calcular">
+
     </form>
 
     <?php
+
+
     if ( $_SERVER['REQUEST_METHOD'] === 'POST' && ! empty( $_POST['ip'] ) ) {
         $ip = new calc_ipv4( $_POST['ip'] );
 
         if( $ip->valida_endereco() ) {
+
             echo '<h2>Configurações de rede para <span style="color: #77bfb7;">' . $_POST['ip'] . '</span> </h2>';
             echo "<pre class='resultado'>";
 
@@ -62,7 +69,9 @@
             echo "<b>Primeiro Host: </b>" . $ip->primeiro_ip() . '<br>';
             echo "<b>Último Host: </b>" . $ip->ultimo_ip() . '<br>';
             echo "<b>Total de IPs:  </b>" . $ip->total_ips() . '<br>';
-            echo "<b>Hosts: </b>" . $ip->ips_rede();
+            echo "<b>Hosts: </b>" . $ip->ips_rede() . '<br>';
+            echo "<b>Status da rede: </b>".$ip->statusRede() . '<br>';
+            echo "<b>Classe da rede: </b>" . $ip->classe();
             echo "</pre>";
         } else {
             echo 'Endereço IPv4 inválido!';
